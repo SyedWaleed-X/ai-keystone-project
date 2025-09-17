7bf75fbeae467d619368ebf11785512a367f947a
SyedWaleed-X <wale3dpr0@gmail.com>
2025-09-17 15:16:43 +0500

    feat: initial commit of user profile

---
+++ user_profile.py
@@ -0,0 +1,11 @@
+
+
+
+def format(userDic):
+
+    print(f"username is {userDic["username"]} and email is {userDic["email"]}")
+
+
+userdic = {"username": "waleed", "email": "wale3dpr0@gmail.com"}
+
+format(userdic)
